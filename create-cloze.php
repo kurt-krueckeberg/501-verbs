e76fc2f6d27cc9c34ea9db8baba50bfc8982ec20
Kurt Krueckeberg <kurt.krueckeberg@gmail.com>
2021-07-09 12:14:31 -0400

    updates

---
+++ create-cloze.php
@@ -0,0 +1,78 @@
+<?php
+/*
+ * Adds definition found $verbs[] (from dict.php) to the input file.
+ *
+ */
+require_once "dict.php";
+require_once "algorithms.php";
+require_once "./SplFileObjectExtended.php";
+
+function format($str)
+{
+   return mb_convert_encoding($str, "UTF-8"); // Convert $output to UTF-8 encoding.
+}
+/*
+ * Returns Associative array: $array['some_verb'] = array(its conjugation)
+ */
+function get_conjugations($fname)
+{
+   $file = new SplFileOjectExtended($fname, "r");
+
+   $conjugations = [];
+ 
+   foreach ($file as $line) {
+      $pos = strpos($line, '|');
+
+      $verb = substr($line, 0, $pos);
+
+      $conj = substr($line, $pos + 1);
+
+      $conj_arr = explode(',', $conj);
+      $conjugations[$verb] = $conj_arr; 
+   }
+   return $conjuations;
+}
+
+function get_verbBlock($file)
+{
+  $infinitive = $file->fgets();
+  if ($infinitive == '') return false;
+ 
+  $sents = [];
+
+  while (($line = $file->fgets()) != '') {
+     
+     $sents[] = $line;      
+  }
+
+  return array($infinitive, $sents);
+}
+
+function transform(string $verb, array $sents, array $conjs)
+{
+
+
+}
+
+function write_out(string $verb, array $new_sents)
+{
+
+}
+
+$ifile = new SplFileObjectExtended("./sentence-results.txt" , "r");
+$ofile = new SplFileObjectExtended("./new-sentence-results.txt" , "w");
+
+ $conjs = get_ conjugations("./german-strong-irr-conjugations.txt");
+
+ $block = get_verbBlock($ifile);
+
+ do {
+
+   list($verb, $sents) = $block;
+
+   $new_sents = transform($verb, $sents, $conjs);
+   write_out($verb, $new_sents);
+
+   $block = get_verbBlock($ifile);
+
+ }  while($block != '')
